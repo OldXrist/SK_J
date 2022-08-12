e44869c2d9b651f9ed155a59e0751d9e2ad3385e
Igor <igor.spiridonovv@bk.ru>
2022-08-12 17:49:28 +0300

    чекбоксы и валидация

--- src/main/resources/scripts/form_data.js
+++ src/main/resources/scripts/form_data.js
@@ -1,1 +1,10 @@
-function validate {}
+function Validate (){
+    for (let i = 1; i <= 8; i++){
+        if (document.getElementById("form" + i).value.length === 0){
+            alert("error");
+        }
+    }
+    if (document.getElementById("check1").checked === false || document.getElementById("check2").checked === false || document.getElementById("check3").checked === false){
+        alert("check error");
+    }
+}
--- src/main/resources/scripts/reg_choice.js
+++ src/main/resources/scripts/reg_choice.js
@@ -3,12 +3,20 @@ function selectOnlyThis(id) {
         document.getElementById("check" + i).checked = false;
     }
 
-    const links = ["reg_uchastnikov/reg_ql.html", "reg_uchastnikov/reg_ip.html", "reg_uchastnikov/reg_fl.html", "reg_organizatorov/reg_org_ql.html", "reg_organizatorov/reg_org_ip.html", "reg_organizatorov/reg_org_fl.html", "reg_organizatorov/reg_org_ay.html"]
+    const links = [
+        "reg_uchastnikov/reg_ql.html",
+        "reg_uchastnikov/reg_ip.html",
+        "reg_uchastnikov/reg_fl.html",
+        "reg_organizatorov/reg_org_ql.html",
+        "reg_organizatorov/reg_org_ip.html",
+        "reg_organizatorov/reg_org_fl.html",
+        "reg_organizatorov/reg_org_ay.html"
+    ]
 
     document.getElementById(id).checked = true;
 
     for (let i = 1; i <= 7; i++){
-        if (id == "check" + i){
+        if (id === "check" + i){
             document.getElementById("next").href = links[i-1];
         }
     }
