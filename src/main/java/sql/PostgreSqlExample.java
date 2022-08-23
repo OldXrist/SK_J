5cf11974f8022d929bd8bd252be9e4b035612a94
mackcik <mackcik228225@gmail.com>
2022-08-23 11:43:52 +0300

    45

--- src/main/java/sql/PostgreSqlExample.java
+++ src/main/java/sql/PostgreSqlExample.java
@@ -1,4 +1,4 @@
-package com.javacodegeeks.example;
+package sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class PostgreSqlExample {
-    private static final String INSERT_NEW="INSERT INTO dish VALUES(?,?)";
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgresql")) {
 
@@ -17,11 +16,11 @@ public class PostgreSqlExample {
             System.out.println("Connected to PostgreSQL database!");
             Statement statement = connection.createStatement();
             System.out.println("Reading car records...");
-            System.out.printf("%-30.30s  %-30.30s%n", "type_users", "role_users");
+            System.out.printf("%-30.30s  %-30.30s%n", "type_users", "role_users", "unn");
             ResultSet resultSet = statement.executeQuery("SELECT * FROM public.reg");
 
             while (resultSet.next()) {
-                System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("type_users"), resultSet.getString("role_users"));
+                System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("type_users"), resultSet.getString("role_users"), resultSet.getString("unn"));
             }
 
         } catch (SQLException e) {
