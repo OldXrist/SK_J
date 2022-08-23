9e46a61275ec62bdaa710179b9cd3d5c2f6effe2
Igor <igor.spiridonovv@bk.ru>
2022-08-23 10:59:54 +0300

    123

--- Servlets/src/main/java/reg/servlets/ULServlet.java
+++ Servlets/src/main/java/reg/servlets/ULServlet.java
@@ -15,8 +15,52 @@ public class ULServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter pw = new PrintWriter("C:\\Users\\manager\\Desktop\\SK_J\\Servlets\\src\\main\\popa.txt", StandardCharsets.UTF_8);
-        String zip = req.getParameter("zip");
-        pw.println(zip);
+        String type_users = req.getParameter("type_users");
+        pw.println(type_users);
+        String role_users = req.getParameter("role_users");
+        pw.println(role_users);
+        String unn = req.getParameter("unn");
+        pw.println(unn);
+        String snils = req.getParameter("snils");
+        pw.println(snils);
+        String reg_nomer_ay = req.getParameter("reg_nomer_ay");
+        pw.println(reg_nomer_ay);
+        String code_ogrn = req.getParameter("code_ogrn");
+        pw.println(code_ogrn);
+        String code_ogrnip = req.getParameter("code_ogrnip");
+        pw.println(code_ogrnip);
+        String famil = req.getParameter("famil");
+        pw.println(famil);
+        String name = req.getParameter("name");
+        pw.println(name);
+        String otch = req.getParameter("otch");
+        pw.println(otch);
+        String poln_naim = req.getParameter("poln_naim");
+        pw.println(poln_naim);
+        String qr_adrs = req.getParameter("qr_adrs");
+        pw.println(qr_adrs);
+        String pocht_adres = req.getParameter("pocht_adres");
+        pw.println(pocht_adres);
+        String naim_org = req.getParameter("naim_org");
+        pw.println(naim_org);
+        String email = req.getParameter("email");
+        pw.println(email);
+        String seria = req.getParameter("seria");
+        pw.println(seria);
+        String nomer = req.getParameter("nomer");
+        pw.println(nomer);
+        String telephon = req.getParameter("telephon");
+        pw.println(telephon);
+        String data_vudochi = req.getParameter("data_vudochi");
+        pw.println(data_vudochi);
+        String kem_vudan = req.getParameter("kem_vudan");
+        pw.println(kem_vudan);
+        String pass = req.getParameter("pass");
+        pw.println(pass);
+        String reg_date = req.getParameter("reg_date");
+        pw.println(reg_date);
+        String last_login = req.getParameter("last_login");
+        pw.println(last_login);
         pw.close();
     }
 }
--- src/main/resources/scripts/validate_ul.js
+++ src/main/resources/scripts/validate_ul.js
@@ -1,40 +1,43 @@
 function Validate_ul (){
     if (document.getElementById("form1").value.length === 0){
-        document.getElementById("form1").value = 'error';
+        document.getElementById("form1").value = 'val';
     }
     if (document.getElementById("form2").value.length === 0){
-        document.getElementById("form2").value = 'error';
+        document.getElementById("form2").value = 'val';
     }
     if (document.getElementById("form3").value.length === 0){
-        document.getElementById("form3").value = 'error';
+        document.getElementById("form3").value = 'val';
     }
     if (document.getElementById("form4").value.length === 0){
-        document.getElementById("form4").value = 'error';
+        document.getElementById("form4").value = 'val';
     }
     if (document.getElementById("form5").value.length === 0){
-        document.getElementById("form5").value = 'error';
+        document.getElementById("form5").value = 'val';
     }
     if (document.getElementById("form6").value.length === 0){
-        document.getElementById("form6").value = 'error';
+        document.getElementById("form6").value = 'val';
     }
     if (document.getElementById("form7").value.length === 0){
-        document.getElementById("form7").value = 'error';
+        document.getElementById("form7").value = 'val';
     }
     if (document.getElementById("form8").value.length === 0){
-        document.getElementById("form8").value = 'error';
+        document.getElementById("form8").value = 'val';
     }
     else if (document.getElementById("check1").checked === false || document.getElementById("check2").checked === false || document.getElementById("check3").checked === false){
         alert("check error");
     }
     else {
         let user = {
-            inn: document.getElementById("form1").value,
-            ogrn: document.getElementById("form2").value,
-            fname: document.getElementById("form3").value,
-            uaddress: document.getElementById("form4").value,
-            zip: document.getElementById("form5").value,
-            phone: document.getElementById("form6").value,
-            email: document.getElementById("form7").value
+            type_users: "Участник",
+            role_users: "ЮЛ",
+            unn: document.getElementById("form1").value,
+            code_ogrn: document.getElementById("form2").value,
+            poln_naim: document.getElementById("form3").value,
+            qr_adrs: document.getElementById("form4").value,
+            pocht_adres: document.getElementById("form5").value,
+            telephon: document.getElementById("form6").value,
+            email: document.getElementById("form7").value,
+            reg_date: Date
         };
 
         let json = JSON.stringify(user);
