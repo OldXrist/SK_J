6e792d84eb37715313cf67ce9e8ca34ef9b92160
Igor <igor.spiridonovv@bk.ru>
2022-08-31 18:02:56 +0300

    ахахахахахахах

--- Servlets/src/main/java/login/servlets/AUTHServlet.java
+++ Servlets/src/main/java/login/servlets/AUTHServlet.java
@@ -16,25 +16,28 @@ public class AUTHServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter pw = new PrintWriter("C:\\Users\\manager\\Desktop\\SK_J\\src\\main\\resources\\scripts\\popa.txt", StandardCharsets.UTF_8);
 
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:63342/SK_J/login.html");
+        res.setHeader("Access-Control-Allow-Methods", "GET");
+
         String email = req.getParameter("email");
         String pwd = req.getParameter("pwd");
 
         try {
             Connection c = DriverManager.getConnection("jdbc:postgresql://192.168.1.115/postgres", "postgres", "postgresql");
 
-            String sql = "SELECT email, pass FROM reg WHERE email = ?";
+            String sql = "SELECT email, pass FROM reg WHERE email = ? AND pass = ?";
             PreparedStatement ps = c.prepareStatement(sql);
 
             ps.setString(1, email);
+            ps.setString(2, pwd);
 
             ResultSet rs = ps.executeQuery();
 
             if (rs.next()) {
-                String x = rs.getString(1);
-                String y = rs.getString(2);
-                pw.println(x);
-                pw.println(y);
+                pw.println(rs.getString(1));
+                pw.println(rs.getString(2));
                 pw.println("success");
+                res.sendRedirect("http://localhost:63342/SK_J/main.html?_ijt=gckjkrah7uctccdva84ir5k84q&_ij_reload=RELOAD_ON_SAVE");
             } else {
                 pw.println("denied");
             }
